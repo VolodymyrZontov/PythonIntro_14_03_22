01ed2a87a39fb7daece0cbcfb8e920f83198a5ff
Volodymyr Zontov <volodymyr.z@cybord.ai>
2022-04-18 19:55:06 +0300

    lesson_11

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print(1, "Hello!")
+persons = [{"name": "david", "age": 2},
+           {"name": "david1234", "age": 5},
+           {"name": "Johna", "age": 15},
+           {"name": "alexander", "age": 2}]
+max_age = persons[0]["age"]
+max_len = len(persons[0]["name"])
+max_age_names = []
+max_len_names = []
+for person in persons:
+    if person["age"] < max_age:
+        name_max_len = person["age"]
+        max_age_names = [person["name"]]
+    elif person["age"] == max_age:
+        max_age_names.append(person["name"])
+    if person["name"] > max_len:
+        name_max_len = person["name"]
+        max_len_names = [person["name"]]
+    elif person["name"] == max_len:
+        max_len_names.append(person["name"])
+
+
